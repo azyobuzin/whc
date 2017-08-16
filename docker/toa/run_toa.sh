41586485d74b9ee4df1a6be16af602e490896489
azyobuzin <azyobuzin@users.sourceforge.jp>
2017-08-17 01:42:30 +0900

    C# 版 Toa 開発停止のお知らせ

--- docker/toa/run_toa.sh
+++ docker/toa/run_toa.sh
@@ -1,2 +1,3 @@
-Xvfb :0 -screen 0 640x394x24 &
-DISPLAY=:0 wine 'C:\Program Files\Mono\bin\mono.exe' 'C:\toa\Toa.exe'
+#!/bin/bash
+
+xvfb-run -as '-screen 0 640x394x24' wine 'C:\Program Files\Mono\bin\mono.exe' 'C:\toa\Toa.exe' -d 'C:\wagahigh'
--- docker/wagahigh/run_wagahigh.sh
+++ docker/wagahigh/run_wagahigh.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 Xvfb :0 -screen 0 640x394x24 &
 x0vncserver -display :0 -SecurityTypes None &
 
-cd /root/.wine/drive_c/wagahigh
+cd ~/.wine/drive_c/wagahigh
 DISPLAY=:0 wine 'ワガママハイスペック.exe'
