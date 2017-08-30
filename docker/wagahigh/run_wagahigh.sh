a32ddfab937ff661c984eae3749d68607b970a31
azyobuzin <azyobuzin@users.sourceforge.jp>
2017-08-30 19:09:34 +0900

    x11vnc を使うことにした

--- docker/wagahigh/run_wagahigh.sh
+++ docker/wagahigh/run_wagahigh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 Xvfb :0 -screen 0 640x394x24 &
-x0vncserver -display :0 -SecurityTypes None &
+x11vnc -display WAIT:0 -shared -forever -q &
+sleep 0.5
 
 cd ~/.wine/drive_c/wagahigh
 DISPLAY=:0 wine 'ワガママハイスペック.exe'
