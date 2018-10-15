4ea7caa9e137bc35f17d2c953dbd8b316a9f7c83
azyobuzin <azyobuzin@users.sourceforge.jp>
2018-10-16 02:04:07 +0900

    自己完結

--- src/Toa.Standalone/DockerBuild.cmd
+++ src/Toa.Standalone/DockerBuild.cmd
@@ -1,4 +1,4 @@
-dotnet publish -c Release
+dotnet publish -c Release -r ubuntu.18.04-x64
 rmdir "..\..\docker\toa\bin" /s /q
-xcopy "bin\Release\netcoreapp2.1\publish" "..\..\docker\toa\bin\" /s
+xcopy "bin\Release\netcoreapp2.1\ubuntu.18.04-x64\publish" "..\..\docker\toa\bin\" /s
 docker build -t toa "..\..\docker\toa"
